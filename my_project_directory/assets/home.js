6ffd1a404e41befb2e0e8688d26bf463bc911087
ibtissemdev <ibtissem.khiri@gmail.com>
2022-08-24 11:54:22 +0200

    fichier js

---
+++ my_project_directory/assets/home.js
@@ -0,0 +1,8 @@
+const form=document.querySelector('#shortenForm');
+const shortenCard=document.querySelector('#shortenCard');
+const inputUrl=document.querySelector('#url');
+const btnShortenUrl=document.querySelector('#btnShortenUrl');
+
+form.addEventListener('submit', function(e) {
+    
+})
