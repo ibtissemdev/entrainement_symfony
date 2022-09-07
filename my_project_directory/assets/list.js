02dbff38c185a4de49b9242f5e6d5c1c81608d18
ibtissemdev <ibtissem.khiri@gmail.com>
2022-09-07 16:38:06 +0200

    Sélectionner des éléments et désélectionner

--- my_project_directory/assets/list.js
+++ my_project_directory/assets/list.js
@@ -1,5 +1,5 @@
 const listGroupItems = document.querySelectorAll('.list-group-item');
-const actions = document.querySelector(('#action'));
+const actions = document.querySelector('#actions');
 
 const btnCopy = document.querySelector('#btnCopy');
 const btnStats = document.querySelector('#btnStats');
@@ -12,10 +12,30 @@ let hash = null;
 
 listGroupItems.forEach(item => {
     item.addEventListener('click', function () {
+        if (selectedItem === this) {
+            selectedItem = null;
+            hash = null;
+            this.classList.remove('active');
+            return;
+        }
+
+        listGroupItems.forEach(item => item.classList.remove('active'));
+
         selectedItem = this;
-        selectedItem.classList.add('active');
         hash = selectedItem.dataset.hash;
+        selectedItem.classList.add('active');
+        toogleButtonsInteraction()
 
     })
-})
+});
+
+btnCopy.addEventListener('click', function(){
+    const link = document.querySelector(`anchor_${hash}`);
+});
+
+const toogleButtonsInteraction = function (isDisabled = false) {
+    Array.from(actions.children).forEach(button => {
+        button.disabled = isDisabled;
+    })
+}
 
